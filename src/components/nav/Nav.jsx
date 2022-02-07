131c60a38f6d471ec78f432ab1c306769626c056
FlyingFool95 <Jhernehult@gmail.com>
2022-02-07 18:08:24 +0100

    made navlinks dynamic from object

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,10 +2,27 @@ import { NavLink } from "react-router-dom"
 import './Nav.css'
 
 export default function Nav() {
+
+  const nav_elements = [
+    {
+      name: "Home",
+      to: "/"
+    },
+    {
+      name: "About",
+      to: "/about"
+    },
+  ]
+
   return (
     <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
+      <div className="desktop_nav">
+        {nav_elements.map(link => (
+          <NavLink key={link.name} to={link.to}>
+            {link.name}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   )
 }
