58aae33c8111df99b88846dee4df40fb38aa750f
FlyingFool95 <Jhernehult@gmail.com>
2022-02-22 19:40:23 +0100

    added check for mobile devices

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,12 @@
-import { useMyContext } from "../../contexts/MyContext"
+import { useIsMobile } from "../../contexts/IsMobileContext"
 
 
 export default function Home() {
-  const { myState } = useMyContext()
+  const { isMobile } = useIsMobile()
   return (
     <div>
       <h1>Home</h1>
       <p>Welcom to my template app. Please use to create future projects!</p>
-      <p>{myState && myState}</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis laborum sequi iste at consequatur enim repudiandae? Similique distinctio dolores commodi!</p>
     </div>
   )
